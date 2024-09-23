71fe36eea520b2d55e6e6571f4df0270879f6bd8
spider6564 <137841167+spider6564@users.noreply.github.com>
2024-09-24 01:23:00 +0530

    aboutme

--- portfolio-page/src/pages/sub-components/About.jsx
+++ portfolio-page/src/pages/sub-components/About.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const About = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [user, setUser] = useState({});
 
-export default About
+  useEffect(() => {
+    const getMyProfile = async () => {
+      const { data } = await axios.get(
+        "http://localhost:4000/api/v1/user/portfolio/me",
+        { withCredentials: true }
+      );
+      setUser(data.user);
+    };
+    getMyProfile();
+  }, []);
+
+  return (
+    <div className="w-full flex flex-col overflow-x-hidden">
+      <div className="relative">
+        <h1
+          className="flex gap-4 items-center text-[2rem] sm:text-[2.5rem] md:text-[3rem] lg:text-[3.8rem] leading-[56px] md:leading-[67px] lg:leading-[90px] tracking-[15px] mx-auto w-fit font-extrabold"
+          style={{ background: "hsl(222.2, 84%, 4.9%)" }}
+        >
+          ABOUT
+          <span className="text-tubeLight-effect font-extrabold">ME</span>
+        </h1>
+        <span className="absolute w-full h-1 top-7 sm:top-7 md:top-8 lg:top-11 z-[-1] bg-slate-200"></span>
+      </div>
+      <div>
+        <div className="grid md:grid-cols-2 my-8 sm:my-20 gap-14">
+          <div className="flex justify-center items-center">
+            <img
+              src={user.avatar && user.avatar.url}
+              alt={user.fullName}
+              className="bg-white p-2 sm:p-4 rotate-[25deg] h-[240px] sm:h-[340px] md:h-[400px] lg:h-[450px]"
+            />
+          </div>
+          <div className="flex justify-center flex-col tracking-[1px] text-xl gap-5">
+            <p>{user.aboutMe}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
